a552e8fdc50f00e6ac702c284cfee25ef068b837
Aarushi Jaiswal <116869331+Aarushii100@users.noreply.github.com>
2023-01-05 21:47:43 +0530

    Create find_the_ original_array_of_ prefix_ xor.cpp

---
+++ find_the_ original_array_of_ prefix_ xor.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    vector<int> findArray(vector<int>& pref) 
+    {
+        vector<int>v;
+        v.push_back(pref[0]);
+        for(int i=1;i<pref.size();i++)
+        {     
+            v.push_back(pref[i-1]^pref[i]);
+        }
+        return v;
+        
+    }
+};
