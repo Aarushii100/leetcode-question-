1ee0c65610d837d4c10bb0b1b085c7dd9ee9e647
Aarushi Jaiswal <116869331+Aarushii100@users.noreply.github.com>
2023-01-19 21:54:01 +0530

    Create Result algo.cpp

---
+++ Result algo.cpp
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<math.h>
+int main()
+{
+int a, int b;
+pritnf("Enter value: ");
+scanf("%d",&a);
+pritnf("Enter value: ");
+scanf("%d",&b);
+int sum = a+b;
+printf("%d",&sum);
+return 0;
+}
